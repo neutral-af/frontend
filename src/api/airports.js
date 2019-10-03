e89b8e68159826fe31be91c62ccfb02700c20241
Jason Gwartz <jason@gwartz.me>
2019-10-04 01:17:30 +0200

    Use fuse.js for fuzzy-match searching

--- src/api/airports.js
+++ src/api/airports.js
@@ -1,13 +1,22 @@
+import Fuse from 'fuse.js'
+
 import airports from '@/airports.json'
 
+const fuse = new Fuse(airports, {
+  keys: [
+    { name: 'name', weight: 0.6 },
+    { name: 'faa', weight: 0.9 }
+    // { name: 'city', weight: 0.4 },
+    // { name: 'country', weight: 0.3 }
+  ]
+})
+
 export const search = (query) => {
-  const keys = ['name', 'city', 'country', 'faa', 'icao']
-  let filtered = airports
-  if (query) {
-    const lowercased = query.toLowerCase()
-    filtered = filtered.filter(airport => keys.some(key => airport[key].toLowerCase().includes(lowercased)))
-  }
-  return filtered.slice(0, 10)
+  if (!query) return []
+
+  const lowercased = query.toLowerCase()
+
+  return fuse.search(lowercased).slice(0, 10)
 }
 
 export const findByIcao = (icao) => airports.find(airport => icao === airport.icao)
