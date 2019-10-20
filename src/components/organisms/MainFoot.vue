9bcdb8beb256feca4d918569567a82dc5c58680a
Emanuele <emanuele.marchi@ecosia.org>
2019-10-21 01:47:09 +0200

    Update main foot

--- src/components/organisms/MainFoot.vue
+++ src/components/organisms/MainFoot.vue
@@ -1,33 +1,19 @@
 <template>
   <div class="container main-foot">
-    <p class="is-small">
-      <RouterLink
-        :to="{ name: 'home' }"
-        class="is-family-secondary is-size-5"
-      >
-        Neutral (as f**k)
-      </RouterLink>
-      <span class="is-block-mobile-small">
-        <span class="is-hidden-mobile-small">·</span>
-      </span>
-    </p>
+    <RouterLink
+      :to="{ name: 'home' }"
+      class="is-family-secondary is-size-5"
+    >
+      Neutral (as f**k)
+    </RouterLink>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .main-foot {
-  font-size: 0.9rem;
-
   a,
   strong {
     color: inherit;
-  }
-
-  a {
-    &:hover,
-    &:focus {
-      text-decoration: underline;
-    }
   }
 }
 </style>
