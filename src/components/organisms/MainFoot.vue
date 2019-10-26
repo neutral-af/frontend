05874295523d88799984e56bb04f8aac9f1d3910
Emanuele <emanuele.marchi@ecosia.org>
2019-10-27 01:06:51 +0200

    Fix footer landmark

--- src/components/organisms/MainFoot.vue
+++ src/components/organisms/MainFoot.vue
@@ -1,34 +1,36 @@
 <template>
-  <div class="container main-foot">
-    <RouterLink
-      :to="{ name: 'home' }"
-      class="is-family-secondary is-size-5"
-    >
-      Neutral (as f**k)
-    </RouterLink>
-    &nbsp;
-    <RouterLink
-      :to="{ name: 'privacy' }"
-      class="main-foot-link"
-    >
-      Privacy policy
-    </RouterLink>
-    <a
-      href="https://www.github.com/neutral-af"
-      target="_blank"
-      rel="noopener"
-      class="main-foot-link"
-    >
-      Source code
-    </a>
-    <a
-      :href="`mailto:${email}`"
-      rel="noopener"
-      class="main-foot-link"
-    >
-      Feedback
-    </a>
-  </div>
+  <footer class="footer main-foot">
+    <div class="container">
+      <RouterLink
+        :to="{ name: 'home' }"
+        class="is-family-secondary is-size-5"
+      >
+        Neutral (as f**k)
+      </RouterLink>
+      &nbsp;
+      <RouterLink
+        :to="{ name: 'privacy' }"
+        class="main-foot-link"
+      >
+        Privacy policy
+      </RouterLink>
+      <a
+        href="https://www.github.com/neutral-af"
+        target="_blank"
+        rel="noopener"
+        class="main-foot-link"
+      >
+        Source code
+      </a>
+      <a
+        :href="`mailto:${email}`"
+        rel="noopener"
+        class="main-foot-link"
+      >
+        Feedback
+      </a>
+    </div>
+  </footer>
 </template>
 
 <script>
