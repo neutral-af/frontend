45f203cc440a833ee17018fde17b643c39b59ce0
Jason Gwartz <jason@gwartz.me>
2019-11-24 16:42:07 +0100

    Adds cookie notice

---
+++ src/components/molecules/CookieNotice.vue
@@ -0,0 +1,17 @@
+<template>
+  <CookieLaw theme="dark-lime">
+    <div slot="message">
+      OK fine, we use cookies - but only if you want to save payment details. Learn more in our
+      <router-link to="privacy">
+        privacy policy
+      </router-link>
+    </div>
+  </CookieLaw>
+</template>
+
+<script>
+import CookieLaw from 'vue-cookie-law'
+export default {
+  components: { CookieLaw }
+}
+</script>
