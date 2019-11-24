41875f866a3d3a505b72745dd95bd75f8be2edfe
Emanuele <emanuele.marchi@ecosia.org>
2019-11-24 17:14:50 +0100

    Move Estimate Checkout state to store and create form molecules (#34)

---
+++ src/components/molecules/NameField.vue
@@ -0,0 +1,28 @@
+<template>
+  <Field
+    label="Cardholder Name"
+    label-for="name"
+    class="name-field"
+    invert
+  >
+    <BInput
+      name="name"
+      size="is-medium"
+      placeholder="Your Cardholder Name"
+      :value="value"
+      required
+      @input="$emit('update', $event)"
+    />
+  </Field>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
