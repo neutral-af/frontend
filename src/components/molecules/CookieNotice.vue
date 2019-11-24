a6c6d53aabb674844a8f9b19c111f4868d3ec609
Jason Gwartz <jason@gwartz.me>
2019-11-24 17:12:21 +0100

    style the cookie notice

--- src/components/molecules/CookieNotice.vue
+++ src/components/molecules/CookieNotice.vue
@@ -1,6 +1,12 @@
 <template>
-  <CookieLaw theme="dark-lime">
-    <div slot="message">
+  <CookieLaw
+    theme="dark-lime"
+    button-class="button is-primary is-normal"
+  >
+    <div
+      slot="message"
+      class="has-text-light is-family-primary"
+    >
       OK fine, we use cookies - but only if you want to save payment details. Learn more in our
       <router-link to="privacy">
         privacy policy
