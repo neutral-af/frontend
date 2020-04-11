4dcfd20e525e3de4d1b2612352e8650d0dba7c96
Emanuele <emanuele.marchi@ecosia.org>
2020-04-11 15:38:07 +0200

    Make BreakdownChart non-reactive, fix PriceInfo

--- src/views/PriceInfo.vue
+++ src/views/PriceInfo.vue
@@ -1,5 +1,6 @@
 <template>
   <div
+    v-if="price"
     ref="chartWrapper"
     class="container pb-4 sm:pb-6 sm:max-w-xl md:max-w-2xl lg:max-w-3xl"
   >
@@ -7,7 +8,7 @@
       Price Estimate Info
     </Title>
     <BreakdownChart
-      :chart-data="breakdown"
+      :data="chartData"
       :options="chartOptions"
       class="mb-6"
     />
@@ -37,15 +38,15 @@ export default {
   },
   computed: {
     ...mapState('estimate', ['price']),
-    value () {
+    breakdown () {
       return this.price.breakdown
     },
-    breakdown () {
+    chartData () {
       return {
-        labels: this.value.map(item => item.name),
+        labels: this.breakdown.map(item => item.name),
         datasets: [{
-          backgroundColor: ['green'],
-          data: this.value.map(item => item.cents / 100)
+          backgroundColor: ['#319795'], // primary-600
+          data: this.breakdown.map(item => item.cents / 100)
         }]
       }
     },
@@ -56,16 +57,32 @@ export default {
         animation: {
           duration: 2000
         },
+        legend: {
+          position: 'bottom',
+          labels: {
+            fontSize: 14,
+            fontFamily: 'Nunito, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"', // font-sans
+            fontColor: 'rgb(74, 85, 104)' // text-gray-700
+          }
+        },
         tooltips: {
+          bodyFontSize: 14,
+          bodyFontFamily: 'Nunito, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"', // font-sans
+          // bodyFontColor: 'rgb(74, 85, 104)', // text-gray-700
           callbacks: {
             label: (item, data) => {
-              const { cents, currency, name } = this.value[item.index]
+              const { cents, currency, name } = this.breakdown[item.index]
               const formatted = format(cents, currency)
               return `${name}: ${formatted}`
             }
           }
         }
       }
+    }
+  },
+  created () {
+    if (!this.price) {
+      this.$router.replace({ name: 'flights', query: this.$route.query })
     }
   }
 }
